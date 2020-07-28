855f25723cbe95efa99977e3c657c3b8a529987d
Zhian N. Kamvar <zkamvar@gmail.com>
2020-07-28 15:51:27 -0700

    use collections and partials

--- electroniq/build.js
+++ electroniq/build.js
@@ -1,28 +1,41 @@
-var metalsmith = require('metalsmith');
-var markdown   = require('metalsmith-markdown');
-var layouts    = require('metalsmith-layouts');
-var handlebars = require('jstransformer-handlebars');
+var metalsmith  = require('metalsmith');
+var markdown    = require('metalsmith-markdown');
+var layouts     = require('metalsmith-layouts');
+var partials    = require('metalsmith-discover-partials');
+var handlebars  = require('jstransformer-handlebars');
+var collections = require('metalsmith-collections');
 
 metalsmith(__dirname)
-.metadata({
-  site: {
-    name: 'Electroniq',
-    description: "Electroniq is astrophysicist (and retro music enthusiast) Tara Himmels' blog."
-  }
-})
-.source('./src')
-.destination('./public')
-.use(markdown())
-.use(layouts({
-        directory: './layouts',
-        default: 'article.hbs',
-        pattern: ["*/*/*html","*/*html","*html"]
-    }))
-.build(function (err) {
-  if (err) {
-    console.log(err);
-  }
-  else {
-    console.log('Electroniq built!');
-  }
-});
+  .metadata({
+    site: {
+      name: 'Electroniq',
+      description: "Electroniq is astrophysicist (and retro music enthusiast) Tara Himmels' blog."
+    }
+  })
+  .source('./src')
+  .destination('./public')
+  .use(partials({
+    directory: './layouts/partials',
+    pattern: /\.hbs$/ // NOTE: regex is unquoted here
+  }))
+  .use(collections({
+    articles: {
+      pattern: 'articles/**/*.md',
+      sortBy: 'date',
+      reverse: true
+    },
+  }))
+  .use(markdown())
+  .use(layouts({
+    directory: './layouts',
+    default: 'article.hbs',
+    pattern: ["*/*/*html","*/*html","*html"]
+  }))
+  .build(function (err) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      console.log('Electroniq built!');
+    }
+  });
---
+++ electroniq/src/articles/spooky-action.md
@@ -0,0 +1,11 @@
+---
+title: "Spooky Action"
+date: 2016-10-14
+blurb: "What would Einstein make of quantum entanglement today?"
+---
+
+## Spooky Action at a Distance
+
+This was featured in a [Science Friday segment]
+
+[Science Friday segment]: https://www.sciencefriday.com/segments/in-the-quantum-world-physics-gets-philosophical/
